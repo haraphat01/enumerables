e1dcb59f48bf3a73d018d1ec378d1f0bb893f033
pencil <haraphat@gmail.com>
2020-06-02 11:20:29 +0100

    added the second method

--- enumerables.rb
+++ enumerables.rb
@@ -4,14 +4,28 @@ module Enumerable
       
       i = 0
       array = to_a
-      while i > array.length
+      while i < array.length
         yield(array[i])
         i+=1
     end
     array
   end
-  a ={ height: '6 ft', weight: '160 lbs' }
-  a.my_each do |key, value|
-    puts "this is the #{"key"}"
-  end 
+ 
+
+def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+    
+    i = 0
+    array = to_a
+    while i < array.length
+      yield(array[i], i)
+      i+=1
+  end
+  array
+end
+
+a ={ height: '6 ft', weight: '160 lbs' }
+a.my_each_with_index do |key, value|
+  puts "this is the #{key} with #{value}"
+end 
 end
